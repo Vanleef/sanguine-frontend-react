1847836f1da1643e18951f98cfbd305b9cb7722a
Bruno Simões Orlandelli <brunosorlandelli@gmail.com>
2022-05-27 18:55:22 -0300

    Visual changes
    add glassmorph to cards and minor elements in the page.

--- src/components/pages/Bancos.js
+++ src/components/pages/Bancos.js
@@ -178,27 +178,38 @@ const example = [
 ]
 
 const Bancos = () => {
-    let estados = []
-    example.map((item) => {
-        if (!estados.includes(item.estado)) {
-            estados.push(item.estado)
-            console.log(estados)
-        }
-    })
-
-
     return (
         <div>
             <div class="dropdownBox" include="form-input-select()">
-                <select required>
-                    <option value="" hidden>Estado</option>
-
-                    {estados.forEach(function (estado, i) {
-                        { console.log(estado, i) }
-                        <option value={i}>{estado}</option>
-
-                    })}
-
+                <select>
+                    <option value='' hidden selected>Selecione o seu estado:</option>
+                    <option value='AC'>Acre</option>
+                    <option value='AL'>Alagoas</option>
+                    <option value='AP'>Amapá</option>
+                    <option value='AM'>Amazonas</option>
+                    <option value='BA'>Bahia</option>
+                    <option value='CE'>Ceará</option>
+                    <option value='ES'>Espirito Santo</option>
+                    <option value='GO'>Goiás</option>
+                    <option value='MA'>Maranhão</option>
+                    <option value='MT'>Mato Grosso</option>
+                    <option value='MS'>Mato Grosso do Sul</option>
+                    <option value='MG'>Minas Gerais</option>
+                    <option value='PA'>Pará</option>
+                    <option value='PB'>Paraíba</option>
+                    <option value='PR'>Paraná</option>
+                    <option value='PE'>Pernambuco</option>
+                    <option value='PI'>Piauí</option>
+                    <option value='RJ'>Rio de Janeiro</option>
+                    <option value='RN'>Rio Grande do Norte</option>
+                    <option value='RS'>Rio Grande do Sul</option>
+                    <option value='RO'>Rondônia</option>
+                    <option value='RR'>Roraima</option>
+                    <option value='SC'>Santa Catarina</option>
+                    <option value='SP'>São Paulo</option>
+                    <option value='SE'>Sergipe</option>
+                    <option value='TO'>Tocantins</option>
+                    <option value='DF'>Distrito Federal</option>
                 </select>
             </div>
             <div id="banco-container" onWheel={onWheel}>

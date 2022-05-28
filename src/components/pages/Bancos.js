5ed7abd8d7670dcd49449ffc2109281d416240bc
Bruno Simões Orlandelli <brunosorlandelli@gmail.com>
2022-05-28 00:31:33 -0300

    Update 'Sobre' page
    - Added FAQ section
    - Added initial card model for developers info

--- src/components/pages/Bancos.js
+++ src/components/pages/Bancos.js
@@ -179,53 +179,54 @@ const example = [
 
 const Bancos = () => {
     return (
-        <div>
-            <div class="dropdownBox" include="form-input-select()">
-                <select>
-                    <option value='' hidden selected>Selecione o seu estado:</option>
-                    <option value='AC'>Acre</option>
-                    <option value='AL'>Alagoas</option>
-                    <option value='AP'>Amapá</option>
-                    <option value='AM'>Amazonas</option>
-                    <option value='BA'>Bahia</option>
-                    <option value='CE'>Ceará</option>
-                    <option value='ES'>Espirito Santo</option>
-                    <option value='GO'>Goiás</option>
-                    <option value='MA'>Maranhão</option>
-                    <option value='MT'>Mato Grosso</option>
-                    <option value='MS'>Mato Grosso do Sul</option>
-                    <option value='MG'>Minas Gerais</option>
-                    <option value='PA'>Pará</option>
-                    <option value='PB'>Paraíba</option>
-                    <option value='PR'>Paraná</option>
-                    <option value='PE'>Pernambuco</option>
-                    <option value='PI'>Piauí</option>
-                    <option value='RJ'>Rio de Janeiro</option>
-                    <option value='RN'>Rio Grande do Norte</option>
-                    <option value='RS'>Rio Grande do Sul</option>
-                    <option value='RO'>Rondônia</option>
-                    <option value='RR'>Roraima</option>
-                    <option value='SC'>Santa Catarina</option>
-                    <option value='SP'>São Paulo</option>
-                    <option value='SE'>Sergipe</option>
-                    <option value='TO'>Tocantins</option>
-                    <option value='DF'>Distrito Federal</option>
-                </select>
-            </div>
-            <div id="banco-container" onWheel={onWheel}>
-                {example.map((item, index) => {
-                    return (
-                        <CardItem
-                            key={index}
-                            text={[<BancoItens item={item} />, <SangueItens item={item.sangue} />]}
-                            label={item.banco}
-                            path='/bancos'
-                        />
-                    )
-                })}
-            </div>
-        </div >
-
+        <body>
+            <div>
+                <div class="dropdownBox" include="form-input-select()">
+                    <select>
+                        <option value='' hidden selected>Selecione o seu estado:</option>
+                        <option value='AC'>Acre</option>
+                        <option value='AL'>Alagoas</option>
+                        <option value='AP'>Amapá</option>
+                        <option value='AM'>Amazonas</option>
+                        <option value='BA'>Bahia</option>
+                        <option value='CE'>Ceará</option>
+                        <option value='ES'>Espirito Santo</option>
+                        <option value='GO'>Goiás</option>
+                        <option value='MA'>Maranhão</option>
+                        <option value='MT'>Mato Grosso</option>
+                        <option value='MS'>Mato Grosso do Sul</option>
+                        <option value='MG'>Minas Gerais</option>
+                        <option value='PA'>Pará</option>
+                        <option value='PB'>Paraíba</option>
+                        <option value='PR'>Paraná</option>
+                        <option value='PE'>Pernambuco</option>
+                        <option value='PI'>Piauí</option>
+                        <option value='RJ'>Rio de Janeiro</option>
+                        <option value='RN'>Rio Grande do Norte</option>
+                        <option value='RS'>Rio Grande do Sul</option>
+                        <option value='RO'>Rondônia</option>
+                        <option value='RR'>Roraima</option>
+                        <option value='SC'>Santa Catarina</option>
+                        <option value='SP'>São Paulo</option>
+                        <option value='SE'>Sergipe</option>
+                        <option value='TO'>Tocantins</option>
+                        <option value='DF'>Distrito Federal</option>
+                    </select>
+                </div>
+                <div id="banco-container" onWheel={onWheel}>
+                    {example.map((item, index) => {
+                        return (
+                            <CardItem
+                                key={index}
+                                text={[<BancoItens item={item} />, <SangueItens item={item.sangue} />]}
+                                label={item.banco}
+                                path='/bancos'
+                            />
+                        )
+                    })}
+                </div>
+            </div >
+        </body>
     );
 
 }

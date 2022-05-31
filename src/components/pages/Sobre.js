8d94f5c48db95912d71628e86a1127d3791d81ac
Mateus Fittipaldi <mateusfitti@gmail.com>
2022-05-31 03:31:48 -0300

    Added HideText component to page 'Sobre' and Footer is hidden for now

--- src/components/pages/Sobre.js
+++ src/components/pages/Sobre.js
@@ -1,5 +1,16 @@
 import React from 'react';
+import HideText from '../HideText';
 import './Sobre.css';
+
+const Faq1 = () => {
+    return (
+        <div class="collapse">
+            <div class="card-body">
+                <p></p>
+            </div>
+        </div>
+    )
+}
 
 const Sobre = () => {
     return (
@@ -17,21 +28,7 @@ const Sobre = () => {
                             </div>
                             <div class="col-md-6 offset-md-3">
                                 <div class="faq" id="accordion">
-                                    <div class="card">
-                                        <div class="card-header">
-                                            <div class="mb-0">
-                                                <h5 class="faq-title">
-                                                    <span class="badge">1</span>Quem pode doar?
-                                                </h5>
-                                            </div>
-                                        </div>
-                                        <div class="collapse">
-                                            <div class="card-body">
-                                                <p>Qualquer pessoa com idade entre 18 e 69 anos que pese mais de 50 quilos. Jovens de 16 ou 17 anos também estão aptos, desde que tenham o consentimento dos pais ou dos responsáveis.</p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="card">
+                                    {/* <div class="card">
                                         <div class="card-header">
                                             <div class="mb-0">
                                                 <h5 class="faq-title">
@@ -44,49 +41,38 @@ const Sobre = () => {
                                                 <p>A coleta do sangue é bem rápida: leva de 8 a 12 minutos. Antes, o doador faz uma triagem e passa algumas informações de saúde para os funcionários do local.</p>
                                             </div>
                                         </div>
-                                    </div>
-                                    <div class="card">
-                                        <div class="card-header">
-                                            <div class="mb-0">
-                                                <h5 class="faq-title">
-                                                    <span class="badge">3</span>O procedimento oferece algum perigo?
-                                                </h5>
-                                            </div>
-                                        </div>
-                                        <div class="collapse">
-                                            <div class="card-body">
-                                                <p>Não. A doação é totalmente segura e não apresenta nenhum risco. Os materiais utilizados, como a agulha e os cateteres, são descartáveis.</p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="card">
-                                        <div class="card-header">
-                                            <div class="mb-0">
-                                                <h5 class="faq-title">
-                                                    <span class="badge">4</span> Quanto sangue é retirado?
-                                                </h5>
-                                            </div>
-                                        </div>
-                                        <div class="collapse">
-                                            <div class="card-body">
-                                                <p>São 450 mililitros. Esse volume pode salvar a vida de até quatro pessoas.</p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="card">
-                                        <div class="card-header">
-                                            <div class="mb-0">
-                                                <h5 class="faq-title">
-                                                    <span class="badge">5</span> Mas meu corpo não vai sentir falta desse sangue?
-                                                </h5>
-                                            </div>
-                                        </div>
-                                        <div class="collapse">
-                                            <div class="card-body">
-                                                <p> Não. Nós possuímos cerca de 5 litros do líquido vermelho circulando pelos vasos. A retirada não prejudica em nada: o organismo repõe e alcança os níveis normais em até 72 horas.</p>
-                                            </div>
-                                        </div>
-                                    </div>
+                                    </div> */}
+                                    <HideText 
+                                        number="1"
+                                        textButton="Quem pode doar?"
+                                        textDisplay="Qualquer pessoa com idade entre 18 e 69 anos que pese mais de 50 quilos. 
+                                        Jovens de 16 ou 17 anos também estão aptos, desde que tenham o consentimento dos pais 
+                                        ou dos responsáveis."
+                                    />
+                                    <HideText
+                                        number="2"
+                                        textButton="Quanto tempo demora?"
+                                        textDisplay="A coleta do sangue é bem rápida: leva de 8 a 12 minutos. Antes, o doador 
+                                        faz uma triagem e passa algumas informações de saúde para os funcionários do local."
+                                    />
+                                    <HideText
+                                        number="3"
+                                        textButton="O procedimento oferece algum perigo?"
+                                        textDisplay="Não. A doação é totalmente segura e não apresenta nenhum risco. Os materiais 
+                                        utilizados, como a agulha e os cateteres, são descartáveis."
+                                    />
+                                    <HideText
+                                        number="4"
+                                        textButton="Quanto sangue é retirado?"
+                                        textDisplay="São 450 mililitros. Esse volume pode salvar a vida de até quatro pessoas."
+                                    />
+                                    <HideText
+                                        number="5"
+                                        textButton="Mas meu corpo não vai sentir falta desse sangue?"
+                                        textDisplay="Não. Nós possuímos cerca de 5 litros do líquido vermelho circulando pelos 
+                                        vasos. A retirada não prejudica em nada: o organismo repõe e alcança os níveis normais 
+                                        em até 72 horas."
+                                    />
                                 </div>
                             </div>
                         </div>
@@ -117,9 +103,9 @@ const Sobre = () => {
                 </section>
 
             </body>
-            <footer>
+            {/* <footer>
                 <h1>Footer</h1>
-            </footer>
+            </footer> */}
         </html >
     );
 }
